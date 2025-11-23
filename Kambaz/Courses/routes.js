a9341a84c60e424282683a5c32ce8b4b678f6654
Rosa Mohammadi <rosamohammadi@mac.mynetworksettings.com>
2025-11-22 22:26:09 -0500

    added check for unauthorized user

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -5,6 +5,10 @@ export default function CourseRoutes(app, db) {
   const enrollmentsDao = EnrollmentsDao(db);
   const createCourse = (req, res) => {
     const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
     const newCourse = dao.createCourse(req.body);
     enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
     res.json(newCourse);
